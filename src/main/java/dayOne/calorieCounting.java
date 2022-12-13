8d2524126f658d28c00cf4efe591589cf0be9e71
sarinaamin <s.amin@elsevier.com>
2022-12-13 11:00:42 +0000

    day 1 + 2 solutions

---
+++ src/main/java/dayOne/calorieCounting.java
@@ -0,0 +1,38 @@
+package dayOne;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class calorieCounting {
+    public static void main(String[] args) {
+        List<String> calories = new ArrayList<>();
+        List<Integer> totalCalories = new ArrayList<>();
+        try (Scanner scanner = new Scanner(Paths.get("src/main/java/dayOne/calories.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("")) {
+                    Integer sum = calories.stream()
+                            .mapToInt(Integer::parseInt)
+                            .sum();
+                    totalCalories.add(sum);
+                    calories.clear();
+                } else {
+                    calories.add(line);
+                }
+            }
+            Integer sum = calories.stream()
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+            totalCalories.add(sum);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Part One: " + Collections.max(totalCalories));
+        System.out.println("Part Two: " + totalCalories.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .mapToInt(x -> x)
+                .sum());
+    }
+}
---
+++ src/main/java/dayTwo/rockPaperScissors.java
@@ -0,0 +1,125 @@
+package dayTwo;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class rockPaperScissors {
+    public static void main(String[] args) {
+        List<String> matches = readFile();
+        int partOneScore = scoreTournament(matches);
+        System.out.println("Part one total score: " + partOneScore);
+        List<String> partTwoMatches = chooseShape(matches);
+        int partTwoScore = scoreTournament(partTwoMatches);
+        System.out.println("Part two total score: " + partTwoScore);
+    }
+
+    private static List<String> readFile() {
+        List<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(Paths.get("src/main/java/dayTwo/strategy.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    private static int scoreTournament(List<String> matches) {
+        int totalScore = 0;
+
+        Map<String, Integer> lose = Map.of(
+                "A Z", 3,
+                "B X", 1,
+                "C Y", 2
+        );
+
+        Map<String, Integer> win = Map.of(
+                "A Y", 2,
+                "B Z", 3,
+                "C X", 1
+        );
+
+        Map<String, Integer> draw = Map.of(
+                "A X", 1,
+                "B Y", 2,
+                "C Z", 3
+        );
+
+        for (String match : matches) {
+            if (win.containsKey(match)) {
+                totalScore += (win.get(match) + 6);
+            }
+
+            if (lose.containsKey(match)) {
+                totalScore += lose.get(match);
+            }
+
+            if (draw.containsKey(match)) {
+                totalScore += (draw.get(match) + 3);
+            }
+        }
+
+        return totalScore;
+    }
+
+    private static List<String> chooseShape(List<String> matches) {
+        List<String> finalMatches = new ArrayList<>();
+
+        String  playerOneMove = "";
+        String playerTwoGoal = "";
+
+        for (String match : matches) {
+            String[] moves = match.split(" ");
+            playerOneMove = moves[0];
+            playerTwoGoal = moves[1];
+
+            String playerTwoMove = "";
+
+            switch(playerTwoGoal) {
+                case "X":
+                    playerTwoMove = losingShape(playerOneMove);
+                    break;
+                case "Y":
+                    playerTwoMove = drawingShape(playerOneMove);
+                    break;
+                case "Z":
+                    playerTwoMove = winningShape(playerOneMove);
+                    break;
+            }
+            finalMatches.add(playerOneMove + " "  +  playerTwoMove);
+        }
+        return finalMatches;
+    }
+
+    private static String losingShape(String playerOneMove){
+        return switch(playerOneMove) {
+            case "A" -> "Z";
+            case "B" -> "X";
+            case "C" -> "Y";
+            default -> throw new IllegalStateException("Unexpected value");
+        };
+    }
+
+    private static String winningShape(String playerOneMove){
+        return switch(playerOneMove) {
+            case "A" -> "Y";
+            case "B" -> "Z";
+            case "C" -> "X";
+            default -> throw new IllegalStateException("Unexpected value");
+        };
+    }
+
+    private static String drawingShape(String playerOneMove) {
+        return switch(playerOneMove) {
+            case "A" -> "X";
+            case "B" -> "Y";
+            case "C" -> "Z";
+            default -> throw new IllegalStateException("Unexpected value");
+        };
+    }
+}
